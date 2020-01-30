28e29db08d5d8a7e67920899395781017f3b2f7e
Daniel <daniel.n96@t-online.de>
2020-01-30 23:50:24 +0100

    initvie in own function

--- src/gui/MainGui.java
+++ src/gui/MainGui.java
@@ -10,26 +10,39 @@ import java.util.ArrayList;
 
 public class MainGui {
 
-    JFrame f;
+    JFrame jFrame;
 
     public MainGui(){
 
-        SynergieFilter synergieFilter = new SynergieFilter();
-        ArrayList<Team> team = synergieFilter.getTeamWithSpecificChampion_String("Nautilus");
-        System.out.println(team.get(335).toString());
+        jFrame = new JFrame("TFT");
 
-        f=new JFrame("TFT");
+        initViewElements(jFrame);
 
-        JTextField tf=new JTextField();
-        tf.setBounds(50,50, 150,30);
+        jFrame.setSize(800,600);
+        jFrame.setLayout(null);
+        jFrame.setVisible(true);
+
+    }
+
+    //initinitViewElements [JFrame] -> [JFrame]
+    public void initViewElements(JFrame jFrame){
+        JTextField iChampion=new JTextField();
+        iChampion.setBounds(50,50, 150,30);
+        jFrame.add(iChampion);
 
         JLabel result = new JLabel("Here will be the Result");
-        result.setBounds(50,100,750,450);
+        result.setBounds(50,100,250,100);
+        jFrame.add(result);
 
         JButton b=new JButton("Click Here");
         b.setBounds(200,50,95,30);
+        jFrame.add(b);
+
 
         final int[] counter = {55};
+        SynergieFilter synergieFilter = new SynergieFilter();
+        ArrayList<Team> team = synergieFilter.getTeamWithSpecificChampion_String("Nautilus");
+        System.out.println(team.get(335).toString());
         b.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 counter[0]++;
@@ -38,15 +51,28 @@ public class MainGui {
             }
         });
 
-        f.add(b);
-        f.add(result);
-        f.add(tf);
-        f.setSize(800,600);
-        f.setLayout(null);
-        f.setVisible(true);
+        //Champion Analyser Section
+        JLabel titleAnalyser = new JLabel("Champion Analyser Section");
+        titleAnalyser.setBounds(150,250,250,100);
+        jFrame.add(titleAnalyser);
+
+        JTextField iAmountOfChampions=new JTextField();
+        iAmountOfChampions.setBounds(120,330, 100,30);
+        jFrame.add(iAmountOfChampions);
+
+        JButton bStart=new JButton("Start");
+        bStart.setBounds(270,330,80,40);
+        jFrame.add(bStart);
+
+        JButton bStop=new JButton("Stop");
+        bStop.setBounds(350,330,80,40);
+        jFrame.add(bStop);
+
+        JLabel lAnalyserResult = new JLabel("Here will be the Result");
+        lAnalyserResult.setBounds(50,100,250,100);
+        jFrame.add(lAnalyserResult);
+
 
     }
-
-
 
 }
