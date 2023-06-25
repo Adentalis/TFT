8067db688f74604f8b63ce6902a648abf5435f01
Adentalis <daniel.n96@t-online.de>
2023-06-26 00:01:14 +0200

    add s9 champions

---
+++ ts/champions.ts
@@ -0,0 +1,471 @@
+const aatrox: Champion = {
+  name: 'Aatrox',
+  cost: 5,
+  origin: ['Darkin'],
+  class: ['Juggernaut', 'Slayer'],
+  range: 1,
+};
+
+const ahri: Champion = {
+  name: 'Ahri',
+  cost: 5,
+  origin: ['Ionia'],
+  class: ['Sorcerer'],
+  range: 4,
+};
+
+const akshan: Champion = {
+  name: 'Akshan',
+  cost: 3,
+  range: 4,
+  origin: ['Shurima'],
+  class: ['Deadeye'],
+};
+
+const aphelios: Champion = {
+  name: 'Aphelios',
+  cost: 4,
+  range: 4,
+  origin: ['Targon'],
+  class: ['Deadeye'],
+};
+
+const ashe: Champion = {
+  name: 'Ashe',
+  cost: 2,
+  range: 4,
+  origin: ['Freljord'],
+  class: ['Deadeye'],
+};
+
+const azir: Champion = {
+  name: 'Azir',
+  cost: 4,
+  range: 4,
+  origin: ['Shurima'],
+  class: ['Strategist'],
+};
+
+const belveth: Champion = {
+  name: "Bel'Veth",
+  cost: 5,
+  range: 3,
+  origin: ['Void'],
+  class: ['Empress'],
+};
+
+const cassiopeia: Champion = {
+  name: 'Cassiopeia',
+  cost: 1,
+  range: 4,
+  origin: ['Noxus', 'Shurima'],
+  class: ['Invoker'],
+};
+
+const chogath: Champion = {
+  name: "Cho'Gath",
+  cost: 1,
+  range: 1,
+  origin: ['Void'],
+  class: ['Bruiser'],
+};
+
+const darius: Champion = {
+  name: 'Darius',
+  cost: 3,
+  range: 1,
+  origin: ['Noxus'],
+  class: ['Juggernaut'],
+};
+
+const ekko: Champion = {
+  name: 'Ekko',
+  cost: 3,
+  range: 1,
+  origin: ['Piltover', 'Zaun'],
+  class: ['Rogue'],
+};
+
+const galio: Champion = {
+  name: 'Galio',
+  cost: 2,
+  range: 1,
+  origin: ['Demacia'],
+  class: ['Invoker'],
+};
+
+const garen: Champion = {
+  name: 'Garen',
+  cost: 3,
+  range: 1,
+  origin: ['Demacia'],
+  class: ['Juggernaut'],
+};
+
+const gwen: Champion = {
+  name: 'Gwen',
+  cost: 4,
+  range: 2,
+  origin: ['Shadow Isles'],
+  class: ['Slayer'],
+};
+
+const heimerdinger: Champion = {
+  name: 'Heimerdinger',
+  cost: 5,
+  range: 4,
+  origin: ['Piltover', 'Yordle'],
+  class: ['Technogenius'],
+};
+
+const irelia: Champion = {
+  name: 'Irelia',
+  cost: 1,
+  range: 1,
+  origin: ['Ionia'],
+  class: ['Challenger'],
+};
+
+const jarvan: Champion = {
+  name: 'Jarvan',
+  cost: 4,
+  range: 1,
+  origin: ['Demacia'],
+  class: ['Strategist'],
+};
+
+const jayce: Champion = {
+  name: 'Jayce',
+  cost: 3,
+  range: 3,
+  origin: ['Piltover'],
+  class: ['Gunner'],
+};
+
+const jhin: Champion = {
+  name: 'Jhin',
+  cost: 1,
+  range: 3,
+  origin: ['Ionia'],
+  class: ['Deadeye'],
+};
+
+const jinx: Champion = {
+  name: 'Jinx',
+  cost: 2,
+  range: 3,
+  origin: ['Zaun'],
+  class: ['Gunner'],
+};
+
+const kSante: Champion = {
+  name: "K'Sante",
+  cost: 5,
+  range: 1,
+  origin: ['Shurima'],
+  class: ['Bastion'],
+};
+
+const kaisa: Champion = {
+  name: "Kai'Sa",
+  cost: 4,
+  range: 4,
+  origin: ['Void'],
+  class: ['Challenger'],
+};
+
+const kalista: Champion = {
+  name: 'Kalista',
+  cost: 3,
+  range: 4,
+  origin: ['Shadow Isles'],
+  class: ['Challenger'],
+};
+
+const karma: Champion = {
+  name: 'Karma',
+  cost: 3,
+  range: 3,
+  origin: ['Ionia'],
+  class: ['Invoker'],
+};
+
+const kassadin: Champion = {
+  name: 'Kassadin',
+  cost: 2,
+  range: 2,
+  origin: ['Void'],
+  class: ['Bastion'],
+};
+
+const katarina: Champion = {
+  name: 'Katarina',
+  cost: 3,
+  range: 2,
+  origin: ['Noxus'],
+  class: ['Rogue'],
+};
+
+const kayle: Champion = {
+  name: 'Kayle',
+  cost: 1,
+  range: 3,
+  origin: ['Demacia'],
+  class: ['Slayer'],
+};
+
+const kled: Champion = {
+  name: 'Kled',
+  cost: 2,
+  range: 1,
+  origin: ['Noxus', 'Yordle'],
+  class: ['Slayer'],
+};
+
+const lissandra: Champion = {
+  name: 'Lissandra',
+  cost: 3,
+  range: 2,
+  origin: ['Freljord'],
+  class: ['Invoker'],
+};
+
+const lux = {
+  name: 'Lux',
+  cost: 4,
+  range: 1,
+  origin: ['Demacia'],
+  class: ['Sorcerer'],
+};
+
+const malzahar = {
+  name: 'Malzahar',
+  cost: 1,
+  range: 1,
+  origin: ['Void'],
+  class: ['Sorcerer'],
+};
+
+const maokai = {
+  name: 'Maokai',
+  cost: 1,
+  range: 1,
+  origin: ['Shadow Isles'],
+  class: ['Bastion'],
+};
+
+const nasus = {
+  name: 'Nasus',
+  cost: 4,
+  range: 1,
+  origin: ['Shurima'],
+  class: ['Juggernaut'],
+};
+
+const orianna = {
+  name: 'Orianna',
+  cost: 1,
+  range: 1,
+  origin: ['Piltover'],
+  class: ['Sorcerer'],
+};
+
+const poppy = {
+  name: 'Poppy',
+  cost: 1,
+  range: 1,
+  origin: ['Demacia', 'Yordle'],
+  class: ['Bastion'],
+};
+
+const rekSai = {
+  name: "Rek'Sai",
+  cost: 3,
+  range: 1,
+  origin: ['Void'],
+  class: ['Bruiser'],
+};
+
+const renekton = {
+  name: 'Renekton',
+  cost: 1,
+  range: 1,
+  origin: ['Shurima'],
+  class: ['Bruiser'],
+};
+
+const ryze = {
+  name: 'Ryze',
+  cost: 5,
+  range: 3,
+  origin: ['Wanderer'],
+  class: ['Invoker'],
+};
+
+const samira = {
+  name: 'Samira',
+  cost: 1,
+  range: 3,
+  origin: ['Noxus'],
+  class: ['Challenger'],
+};
+
+const sejuani = {
+  name: 'Sejuani',
+  cost: 4,
+  range: 1,
+  origin: ['Freljord'],
+  class: ['Bruiser'],
+};
+
+const senna = {
+  name: 'Senna',
+  cost: 5,
+  range: 3,
+  origin: ['Shadow Isles'],
+  class: ['Gunner', 'Redeemer'],
+};
+
+const sett = {
+  name: 'Sett',
+  cost: 2,
+  range: 1,
+  origin: ['Ionia'],
+  class: ['Juggernaut'],
+};
+
+const shen = {
+  name: 'Shen',
+  cost: 4,
+  range: 1,
+  origin: ['Ionia'],
+  class: ['Bastion', 'Invoker'],
+};
+
+const sion = {
+  name: 'Sion',
+  cost: 5,
+  range: 1,
+  origin: ['Noxus'],
+  class: ['Bruiser'],
+};
+
+const sona = {
+  name: 'Sona',
+  cost: 3,
+  range: 4,
+  origin: ['Demacia'],
+  class: ['Multicaster'],
+};
+
+const soraka = {
+  name: 'Soraka',
+  cost: 2,
+  range: 4,
+  origin: ['Targon'],
+  class: ['Invoker'],
+};
+
+const swain = {
+  name: 'Swain',
+  cost: 2,
+  range: 2,
+  origin: ['Noxus'],
+  class: ['Sorcerer', 'Strategist'],
+};
+
+const taliyah = {
+  name: 'Taliyah',
+  cost: 2,
+  range: 4,
+  origin: ['Shurima'],
+  class: ['Multicaster'],
+};
+
+const taric = {
+  name: 'Taric',
+  cost: 3,
+  range: 1,
+  origin: ['Targon'],
+  class: ['Bastion', 'Sorcerer'],
+};
+
+const teemo = {
+  name: 'Teemo',
+  cost: 2,
+  range: 4,
+  origin: ['Yordle'],
+  class: ['Multicaster', 'Strategist'],
+};
+
+const tristana = {
+  name: 'Tristana',
+  cost: 1,
+  range: 4,
+  origin: ['Yordle'],
+  class: ['Gunner'],
+};
+
+const urgot = {
+  name: 'Urgot',
+  cost: 4,
+  range: 1,
+  origin: ['Zaun'],
+  class: ['Deadeye'],
+};
+
+const velkoz = {
+  name: "Vel'Koz",
+  cost: 3,
+  range: 4,
+  origin: ['Void'],
+  class: ['Multicaster', 'Sorcerer'],
+};
+
+const vi = {
+  name: 'Vi',
+  cost: 2,
+  range: 1,
+  origin: ['Piltover'],
+  class: ['Bruiser'],
+};
+
+const viego = {
+  name: 'Viego',
+  cost: 1,
+  range: 1,
+  origin: ['Shadow Isles'],
+  class: ['Rogue'],
+};
+
+const warwick = {
+  name: 'Warwick',
+  cost: 2,
+  range: 1,
+  origin: ['Zaun'],
+  class: ['Challenger', 'Juggernaut'],
+};
+
+const yasuo = {
+  name: 'Yasuo',
+  cost: 4,
+  range: 1,
+  origin: ['Ionia'],
+  class: ['Challenger'],
+};
+
+const zed = {
+  name: 'Zed',
+  cost: 2,
+  range: 1,
+  origin: ['Ionia'],
+  class: ['Rogue', 'Slayer'],
+};
+
+const zeri = {
+  name: 'Zeri',
+  cost: 4,
+  range: 4,
+  origin: ['Zaun'],
+  class: ['Gunner'],
+};
--- ts/interfaces.ts
+++ ts/interfaces.ts
@@ -3,7 +3,7 @@ interface Team {
 }
 
 interface Champion {
-  name: ChampionName | string;
+  name: ChampionName;
   cost: 1 | 2 | 3 | 4 | 5;
   range: 1 | 2 | 3 | 4 | 5;
   origin: Origin[];
@@ -43,4 +43,63 @@ type Origin =
   | 'Yordle'
   | 'Zaun';
 
-type ChampionName = 'Tristana' | 'Heimerdinger' | 'Jhin';
+type ChampionName =
+  | 'Aatrox'
+  | 'Ahri'
+  | 'Akshan'
+  | 'Aphelios'
+  | 'Ashe'
+  | 'Azir'
+  | "Bel'Veth"
+  | 'Cassiopeia'
+  | "Cho'Gath"
+  | 'Darius'
+  | 'Ekko'
+  | 'Galio'
+  | 'Garen'
+  | 'Gwen'
+  | 'Heimerdinger'
+  | 'Irelia'
+  | 'Jarvan'
+  | 'Jayce'
+  | 'Jhin'
+  | 'Jinx'
+  | "K'Sante"
+  | "Kai'Sa"
+  | 'Kalista'
+  | 'Karma'
+  | 'Kassadin'
+  | 'Katarina'
+  | 'Kayle'
+  | 'Kled'
+  | 'Lissandra'
+  | 'Lux'
+  | 'Malzahar'
+  | 'Maokai'
+  | 'Nasus'
+  | 'Orianna'
+  | 'Poppy'
+  | "Rek'Sai"
+  | 'Renekton'
+  | 'Ryze'
+  | 'Samira'
+  | 'Sejuani'
+  | 'Senna'
+  | 'Sett'
+  | 'Shen'
+  | 'Sion'
+  | 'Sona'
+  | 'Soraka'
+  | 'Swain'
+  | 'Taliyah'
+  | 'Taric'
+  | 'Teemo'
+  | 'Tristana'
+  | 'Urgot'
+  | "Vel'Koz"
+  | 'Vi'
+  | 'Viego'
+  | 'Warwick'
+  | 'Yasuo'
+  | 'Zed'
+  | 'Zeri';
